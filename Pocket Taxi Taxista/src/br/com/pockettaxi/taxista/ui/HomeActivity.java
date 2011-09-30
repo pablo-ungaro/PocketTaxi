7efb3ee39ec32b46ac74309836762b76a59e5726
unknown <pablo.ungaro@.ideais.local>
2011-09-30 18:35:29 -0300

    Inicio do uso de sharedPreferences para armazenar login

--- Pocket Taxi Taxista/src/br/com/pockettaxi/taxista/ui/HomeActivity.java
+++ Pocket Taxi Taxista/src/br/com/pockettaxi/taxista/ui/HomeActivity.java
@@ -2,17 +2,26 @@ package br.com.pockettaxi.taxista.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import br.com.pockettaxi.taxista.R;
 
-public class HomeActivity extends Activity {    
+public class HomeActivity extends Activity { 
+    public static final String PREFS_NAME = "loginData";
+
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+        //Seta o login se é a primeira vez
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME,0);
+        if (prefs.getLong("login", -1) < 0) {
+            startActivity(new Intent(this, SetupLoginActivity.class));
+        }
 		
 		setContentView(R.layout.main);
 		
---
+++ Pocket Taxi Taxista/src/br/com/pockettaxi/taxista/ui/SetupLoginActivity.java
@@ -0,0 +1,13 @@
+package br.com.pockettaxi.taxista.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SetupLoginActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+
+}
